bc40543abcc4ab8e938527143fa4004051189945
Lycos B Blanza <lycos_blanza@dlsl.edu.ph>
2024-09-20 20:17:30 +0800

    Activity Commit

---
+++ abstraction-and-encapsulation-activity.cpp
@@ -0,0 +1,137 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+// Abstract Account class for encapsulating account operations
+class Account {
+protected:
+    double balance;
+
+public:
+    Account() : balance(0) {}
+    
+    virtual void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            cout << "Deposited: " << amount << "\nNew Balance: " << balance << endl;
+        } else {
+            cout << "Invalid deposit amount." << endl;
+        }
+    }
+
+    virtual void withdraw(double amount) = 0;
+    virtual void checkBalance() const {
+    	cout << "------------------------------" << endl;
+        cout << "Current Balance: " << balance << endl;
+    }
+};
+
+// SavingsAccount class with a minimum balance requirement
+class SavingsAccount : public Account {
+private:
+    const double minimumBalance = 1000;
+
+public:
+    void withdraw(double amount) override {
+        if (amount <= 0) {
+            cout << "Invalid withdrawal amount." << endl;
+        } else if (balance - amount < minimumBalance) {
+            cout << "Cannot withdraw. Minimum balance of " << minimumBalance << " must be maintained." << endl;
+        } else {
+            balance -= amount;
+            cout << "Withdrew: " << amount << "\nNew Balance: " << balance << endl;
+        }
+    }
+};
+
+// CurrentAccount class with no minimum balance requirement
+class CurrentAccount : public Account {
+public:
+    void withdraw(double amount) override {
+        if (amount <= 0) {
+            cout << "Invalid withdrawal amount." << endl;
+        } else if (amount > balance) {
+            cout << "Insufficient funds." << endl;
+        } else {
+            balance -= amount;
+            cout << "Withdrew: " << amount << ". New Balance: " << balance << endl;
+        }
+    }
+};
+
+// Menu Class: Displays the various menus for user interaction
+class Menu {
+public:
+    void showMainMenu() {
+        cout << "\nMAIN MENU: \n1. Savings Account\n2. Current Account\n3. Exit\nChoose [1/2/3]: ";
+    }
+
+    void showSubMenu() {
+    	cout << "------------------------------";
+        cout << "\nSUB MENU: \n1. Deposit\n2. Withdraw\n3. Check Balance\n4. Back\nChoose [1/2/3/4]: ";
+    }
+};
+
+int main() {
+    Menu menu;
+    int mainOption, subOption;
+    SavingsAccount savings;
+    CurrentAccount current;
+	cout << "[B A N K  S Y S T E M]" << endl;
+    do {
+        menu.showMainMenu();
+        cin >> mainOption;
+
+        switch (mainOption) {
+            case 1: {  // Savings Account
+                do {
+                    menu.showSubMenu();
+                    cin >> subOption;
+                    if (subOption == 1) {
+                        double amount;
+                        cout << "------------------------------" << endl;
+                        cout << "Enter deposit amount: "; cin >> amount;
+                        savings.deposit(amount);
+                    } else if (subOption == 2) {
+                        double amount;
+                        cout << "------------------------------" << endl;
+                        cout << "Enter withdrawal amount: "; cin >> amount;
+                        savings.withdraw(amount);
+                    } else if (subOption == 3) {
+                        savings.checkBalance();
+                    }
+                } while (subOption != 4);
+                break;
+            }
+            case 2: {  // Current Account
+                do {
+                    menu.showSubMenu();
+                    cin >> subOption;
+                    if (subOption == 1) {
+                        double amount;
+                        cout << "------------------------------" << endl;
+                        cout << "Enter deposit amount: "; cin >> amount;
+                        current.deposit(amount);
+                    } else if (subOption == 2) {
+                        double amount;
+                        cout << "------------------------------" << endl;
+                        cout << "Enter withdrawal amount: "; cin >> amount;
+                        current.withdraw(amount);
+                    } else if (subOption == 3) {
+                        current.checkBalance();
+                    }
+                } while (subOption != 4);
+                break;
+            }
+            case 3: {
+                cout << "Exiting..." << endl;
+                break;
+            }
+            default:
+                cout << "Invalid option. Try again." << endl;
+        }
+    } while (mainOption != 3);
+
+    return 0;
+}
